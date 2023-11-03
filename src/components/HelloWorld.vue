62f3f0e59afb0ebc4260a5279913bfb84b82037d
huangyu <598050554@qq.com>
2023-11-03 15:37:19 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+<script lang="ts" setup>
+//
+import { ref } from 'vue'
+import djiMavic3 from '@/assets/dji-mavic3.png'
+import djiAri3 from '@/assets/dji-ari3.png'
+import djiMini3 from '@/assets/dji-mini3.png'
+import djiAri3_1 from '@/assets/ari3_1.jpg'
+import djiAri3_2 from '@/assets/ari3_2.jpg'
+import djiMavic3_1 from '@/assets/mavic3pro_1.jpg'
+import djiMavic3_2 from '@/assets/mavic3pro_2.jpg'
+import djiMini3_1 from '@/assets/mini3_1.jpg'
+import djiMini3_2 from '@/assets/mini3_2.jpg'
+
+const cards = [
+  {
+    title: 'Mavic 3 Pro',
+    imgsrc: djiMavic3,
+    flex: 12,
+    height: 200,
+    imgSrcs: [
+      djiMavic3_1,
+      djiMavic3_2,
+    ],
+  },
+  {
+    title: 'Ari 3',
+    imgsrc: djiAri3,
+    flex: 6,
+    height: 160,
+    imgSrcs: [
+      djiAri3_1,
+      djiAri3_2,
+    ],
+  },
+  {
+    title: 'Mini 3',
+    imgsrc: djiMini3,
+    flex: 6,
+    height: 160,
+    imgSrcs: [
+      djiMini3_1,
+      djiMini3_2,
+    ],
+  },
+]
+const window = ref<number>(0)
+
+function cardClick(index: number) {
+  window.value = 0
+  imgSrcs.value = cards[index].imgSrcs
+}
+const imgSrcs = ref(cards[0].imgSrcs)
+
+function sum(nums: Array<number>) {
+  const n = nums.length
+  const left = [...nums]
+  const right = [...nums]
+  for (let i = 0; i < n; i++)
+    left[i] = Math.min(left[i - 1], left[i])
+
+  for (let i = n - 2; i >= 0; i--)
+    right[i] = Math.min(right[i + 1], right[i])
+
+  const inf = Number.POSITIVE_INFINITY; let best = inf
+  for (let i = 0; i < n - 1; i++) {
+    if (left[i] < nums[i] && right[i] < nums[i])
+      best = Math.min(best, left[i] + nums[i] + right[i])
+  }
+  return best == inf ? -1 : best
+}
+</script>
+
+<template>
+  <v-container>
+    <v-responsive>
+      <v-row>
+        <v-col :cols="4">
+          <v-card
+            class="mx-auto"
+            max-width="500"
+          >
+            <v-row dense>
+              <v-col
+                v-for="(card, index) in cards"
+                :key="index"
+                :cols="card.flex"
+              >
+                <v-card
+                  theme="dark"
+                  @click="cardClick(index)"
+                >
+                  <v-img
+                    class="align-end"
+                    :height="card.height"
+                    :src="card.imgsrc"
+                    cover
+                  >
+                    <v-card-title class="text-white">
+                      {{ card.title }}
+                    </v-card-title>
+                  </v-img>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn size="small" color="surface-variant" variant="text" icon="mdi-heart" />
+                    <v-btn size="small" color="surface-variant" variant="text" icon="mdi-bookmark" />
+                    <v-btn size="small" color="surface-variant" variant="text" icon="mdi-share-variant" />
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col :cols="8">
+          <v-window
+            v-model="window"
+            show-arrows
+          >
+            <v-window-item
+              v-for="(src, index) in imgSrcs" :key="index"
+            >
+              <v-card>
+                <v-img :height="500" :src="src" />
+              </v-card>
+            </v-window-item>
+          </v-window>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+//
+</script>
+
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon />
+    <v-app-bar-title />
+  </v-app-bar>
+</template>
